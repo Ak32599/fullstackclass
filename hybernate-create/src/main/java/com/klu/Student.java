48a1323296632a427d6b5c38a727ed61019a43d0
Ak32599 <akhilesh.rachapudi98@gmail.com>
2025-12-31 10:52:16 +0530

    22-12-2025

---
+++ hybernate-create/src/main/java/com/klu/Student.java
@@ -0,0 +1,24 @@
+package com.klu;
+import javax.persistence.*;
+@Entity
+@Table(name="student") // here existing name table will be created=
+public class Student {
+	@Id //this is considered as a primary key
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	
+	public Student()
+	{
+		System.out.print(name);
+	}
+	public Student(String name)
+	{
+		this.name=name;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
